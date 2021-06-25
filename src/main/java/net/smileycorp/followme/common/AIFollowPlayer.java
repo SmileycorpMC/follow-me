b3f2064bfcb555fc44ad6c3b87618660fcb62353
Smileycorp <callum.j.longbottom@gmail.com>
2021-06-25 15:17:25 +0100

    Update AIFollowPlayer.java

--- src/main/java/net/smileycorp/followme/common/AIFollowPlayer.java
+++ src/main/java/net/smileycorp/followme/common/AIFollowPlayer.java
@@ -14,6 +14,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 import net.smileycorp.atlas.api.util.DirectionUtils;
 
