1268a7878e79a4be9448ab7f93bd9cb9719903f8
Smileycorp <callum.j.longbottom@gmail.com>
2021-10-30 17:23:38 +0100

    Update ModDefinitions.java

--- src/main/java/net/smileycorp/followme/common/ModDefinitions.java
+++ src/main/java/net/smileycorp/followme/common/ModDefinitions.java
@@ -7,12 +7,12 @@ import net.minecraft.util.text.TextFormatting;
 import net.smileycorp.followme.common.ai.AIFollowPlayer;
 
 public class ModDefinitions {
-	
+
 	public static final String modid = "followme";
 	public static final String name = "Follow Me";
-	public static final String version = "1.1.4a";
-	public static final String dependencies = "required-after:atlaslib@1.1.3d;";
-	
+	public static final String version = "1.1.5";
+	public static final String dependencies = "required-after:atlaslib@1.1.5;";
+
 	public static ITextComponent getFollowText(String str, AIFollowPlayer task) {
 		ITextComponent result = new TextComponentTranslation("message.followme."+str, new Object[]{task.getEntity().getName(), task.getUser().getName()});
 		result.setStyle(new Style().setColor(TextFormatting.AQUA));
